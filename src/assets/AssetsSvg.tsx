edd6dce9208eb740864f3a4b1d717051ce59ce0c
Rafael <carbonera.rafael@gmail.com>
2026-05-12 20:14:47 -0300

    comentando

--- src/assets/AssetsSvg.tsx
+++ src/assets/AssetsSvg.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 
 // SVG do Nome
 export function NomeSvg() {
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { NomeWhiteSvg, LogoNomeWhiteSvg } from '../../assets/AssetsSvg'
+import { LogoNomeWhiteSvg } from '../../assets/AssetsSvg'
 import './Footer.css'
 
 const nav = [
